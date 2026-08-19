cfe15c33d3bf7f7fd30b3f8442bfec46b0e7e645
phmphongg <phonglong0511@gmail.com>
2026-08-19 16:41:18 +0700

    feat: add NumberAnalyzer program

--- NumberAnalyzer.java
+++ NumberAnalyzer.java
@@ -1,1 +1,22 @@
+public class NumberAnalyzer {
 
+    public static int countEvenNumbers(int[] numbers) {
+        int count = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+
+        int result = countEvenNumbers(numbers);
+
+        System.out.println("So luong so chan: " + result);
+    }
+}
